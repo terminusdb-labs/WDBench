f6c7b08e43761ea8b64175e505027b13a6399949
GavinMendelGleason <ubuntu@ip-172-31-40-165.eu-central-1.compute.internal>
2023-07-27 16:47:15 +0000

    Adding changes to benchmark

--- Execution/terminusdb_benchmark.py
+++ Execution/terminusdb_benchmark.py
@@ -8,8 +8,8 @@ from terminusdb_client import WOQLQuery as wq
 
 GRAPHQL_ENDPOINT="http://localhost:6363/api/graphql/admin/wdbench"
 
-GRAPHQL_FILES=[] #["Queries/GraphQL/single_bgps.txt",
-                 #"Queries/GraphQL/multiple_bgps.txt"]
+GRAPHQL_FILES=["Queries/GraphQL/single_bgps.txt",
+              "Queries/GraphQL/multiple_bgps.txt"]
 
 # Instantiate the client with an endpoint.
 auth = HTTPBasicAuth('admin', 'root')
@@ -28,25 +28,25 @@ for gf in GRAPHQL_FILES:
                 print(f".{n}")
                 # Synchronous request
                 start_time = time.time()
-                data = client.execute(query=query)
-                elapsed_time = int((time.time() - start_time) * 1000)
-                if 'errors' in data:
-                    print(data)
-                else:
-                    results = len(data['data']['Node'])
-                    csvwriter.writerow((n,elapsed_time,results))
-                    print(f"..results: {results}")
-                    tf.flush()
+                try:
+                    data = client.execute(query=query)
+                    elapsed_time = int((time.time() - start_time) * 1000)
+                    if 'errors' in data:
+                        print(data)
+                    else:
+                        results = len(data['data']['Node'])
+                        csvwriter.writerow((n,elapsed_time,results))
+                        print(f"..results: {results}")
+                        tf.flush()
+                except Exception as e:
+                    print(e)
 
 client = Client("http://localhost:6363/")
 client.connect(db="wdbench")
 
-WOQL_FILES=[#"Queries/WOQL/multiple_bgps.txt",
+WOQL_FILES=["Queries/WOQL/single_bgps.txt",
+            "Queries/WOQL/multiple_bgps.txt",
             "Queries/WOQL/paths.txt"]
-
-#WOQL_FILES=["Queries/WOQL/single_bgps.txt",
-#            "Queries/WOQL/multiple_bgps.txt",
-#            "Queries/WOQL/paths.txt"]
 for wf in WOQL_FILES:
     with open(wf, 'r') as qf:
         with open(f"{wf}.times",'w') as tf:
@@ -58,9 +58,12 @@ for wf in WOQL_FILES:
                 print(f".{n}")
                 # Synchronous request
                 start_time = time.time()
-                data = client.query(query)
-                elapsed_time = int((time.time() - start_time) * 1000)
-                results = len(data['bindings'])
-                print(f"..results: {results}")
-                csvwriter.writerow((n,elapsed_time,results))
-                tf.flush()
+                try:
+                    data = client.query(query)
+                    elapsed_time = int((time.time() - start_time) * 1000)
+                    results = len(data['bindings'])
+                    print(f"..results: {results}")
+                    csvwriter.writerow((n,elapsed_time,results))
+                    tf.flush()
+                except Exception as e:
+                    print(e)
